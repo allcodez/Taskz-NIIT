a2d71bc2cf7cb5af33f97f6a7ddcb64be722f00b
Amara-chi <namarachi525@gmail.com>
2024-05-04 10:19:56 +0100

    Progress

--- src/components/task/Minitaskitem.jsx
+++ src/components/task/Minitaskitem.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './minitaskitem.css';
 import { GrStatusGood } from "react-icons/gr";
-import { CiCalendarDate, CiClock2 } from "react-icons/ci";
-import { MdClose } from "react-icons/md";
+import { CiCalendarDate, CiClock2, CiPlay1 } from "react-icons/ci";
+import { MdClose, MdDeleteForever } from "react-icons/md";
 import Popup from 'reactjs-popup';
+import Calendar from '../calendar/Calendar';
 
-export default function Minitaskitem({ taskTitle, time, category }) {
+export default function Minitaskitem({ taskTitle, time, category, month, day }) {
+
   return (
     <Popup
       trigger={(
@@ -28,7 +30,7 @@ export default function Minitaskitem({ taskTitle, time, category }) {
           </div>
         </div>
       )}
-      position="right center"
+      // position="right center"
       modal
       nested
       closeOnDocumentClick
@@ -36,10 +38,56 @@ export default function Minitaskitem({ taskTitle, time, category }) {
       {close => (
         <div className='darkBg'>
           <div className="centered">
-            <div className="close-icon" onClick={close}>
-              <MdClose />
+            <div className="modal">
+              <div className="icon-row">
+                <div className="categ">
+                  <span className='ash'>#</span>
+                  <p>{category}work</p>
+                </div>
+                <div className="options">
+                  <div className='calendar'>
+                    <Popup 
+                    trigger={
+                    <p className='start' onClick={close}>Start: {month}May {day}4</p>}
+                    // nested
+                    closeOnDocumentClick >
+                      {close => (   
+                        <div className="Content">
+                         <Calendar className="calendarDate"/>
+                        </div>
+                      )}
+                    </Popup>
+                  </div>
+                  <div className='due'>
+                    <Popup 
+                    trigger={
+                    <p onClick={close} className='due'>Due</p>}
+                    // nested
+                    closeOnDocumentClick >
+                      {close => (   
+                        <div className="Content">
+                         <Calendar className="dueDate"/>
+                        </div>
+                      )}
+                    </Popup>
+                  </div>
+                  <p className='delete'>Delete task <MdDeleteForever /></p>
+                  <MdClose onClick={close} className='closer'/>
+                </div>
+              </div>
+              <div className="taskArea">
+                <div className="planRow">
+                  <div className="taskInfo">
+                  <GrStatusGood />
+                  {taskTitle}Wash the school
+                  </div>
+                  <div className="taskDurations">
+                    <CiPlay1 />
+                    
+                  </div>
+                </div>
+              </div>
             </div>
-            <p>Popup content here !!</p>
           </div>
         </div>
       )}
